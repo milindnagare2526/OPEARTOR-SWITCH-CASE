9b1349714479dc1286fd1f6af2aeff4925ad2826
milindnagare2526 <milindnagare2526@ternaengg.ac.in>
2026-02-16 09:23:19 +0530

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,30 @@
+// JavaScript Program for Arithmetic Operations using Switch Case
+
+// Taking input from user
+let num1 = parseFloat(prompt("Enter first number:"));
+let num2 = parseFloat(prompt("Enter second number:"));
+let operator = prompt("Enter operator (+, -, *):");
+
+let result;
+
+switch (operator) {
+    case "+":
+        result = num1 + num2;
+        console.log("Addition: " + result);
+        break;
+
+    case "-":
+        result = num1 - num2;
+        console.log("Subtraction: " + result);
+        break;
+
+    case "*":
+        result = num1 * num2;
+        console.log("Multiplication: " + result);
+        break;
+
+    default:
+        console.log("Invalid operator");
+}
+
+alert("Result: " + result);
